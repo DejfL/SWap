0e7dcbcab44e8c140ef040271e3d643ebd97c4bd
DejfL <dawidszymczak92@gmail.com>
2020-08-27 20:13:56 +0200

    dalsze praace, oferta

--- assets/js/moje.js
+++ assets/js/moje.js
@@ -17,7 +17,36 @@ $(".menuItem").click(function() {
         $('html, body').animate({
             scrollTop: $("#two").offset().top - $headerHeight
         }, 1000);
+    } else if ($id == "scrol4") {
+        $('html, body').animate({
+            scrollTop: $("#three").offset().top - $headerHeight
+        }, 1000);
     }
+})
 
+$(".hovereffect").click(function() {
+    var $id = $(this).attr('id');
+    var $headerHeight = $("#header").height();
 
+    if ($id == "hoverWapno1") {
+        if (!$("#wapno2").hasClass("collapse")) {
+            $("#wapno2").addClass("collapse");
+        }
+
+        $("#wapno1").removeClass("collapse");
+
+        $('html, body').animate({
+            scrollTop: $("#wapno1").offset().top - $headerHeight
+        }, 1000);
+    } else if ($id == "hoverWapno2") {
+        if (!$("#wapno1").hasClass("collapse")) {
+            $("#wapno1").addClass("collapse");
+        }
+
+        $("#wapno2").removeClass("collapse");
+
+        $('html, body').animate({
+            scrollTop: $("#wapno2").offset().top
+        }, 1000);
+    }
 })
